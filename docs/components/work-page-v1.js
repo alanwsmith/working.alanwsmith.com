e8a4919d1d7ce4260d19b06d74035594138a848d
Alan Smith <commit@alanwsmith.com>
2025-02-06 21:10:37 -0500

    Starting to make pages

--- docs/components/work-page-v1.js
+++ docs/components/work-page-v1.js
@@ -1,10 +1,33 @@
 const componentSheet = new CSSStyleSheet();
-componentSheet.replaceSync(``);
+componentSheet.replaceSync(`
+:host {
+  display: inline-block;
+  padding: 0.2rem;
+}
+
+.page-wrapper {
+  height: 100%;
+  background: #987;
+}
+
+`);
 
 const template = document.createElement('template');
-template.innerHTML = `<div>ping</div>`;
+template.innerHTML = `<div class="page-wrapper">x</div>`;
 
 class WorkPage extends HTMLElement {
+
+  static instances = {};
+
+  static deregister(el) {
+    // TODO: Remove the element
+  }
+
+  static register(el) {
+    // console.log(`registered: ${el.id}`);
+    this.instances[el.id] = el;
+  }
+
   constructor() {
     super();
   }
@@ -14,6 +37,11 @@ class WorkPage extends HTMLElement {
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.adoptedStyleSheets = [componentSheet];
+    this.constructor.register(this);
+  }
+
+  disconnectedCallback() {
+    this.constructor.deregister(this);
   }
 
 }
