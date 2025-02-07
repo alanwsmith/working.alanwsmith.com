1d7963ea1318b6e06116213aacbb8632578cab82
Alan Smith <commit@alanwsmith.com>
2025-02-07 14:18:20 -0500

    Writing all cards to start with

--- docs/components/work-page-v1.js
+++ docs/components/work-page-v1.js
@@ -839,6 +839,7 @@ class WorkPage extends HTMLElement {
     this.wrapper = this.shadowRoot.querySelector('.wrapper');
     this.content = this.shadowRoot.querySelector('.content');
     this.constructor.register(this);
+    this.completedFirstWrite = false;
   }
 
   disconnectedCallback() {
@@ -859,16 +860,23 @@ class WorkPage extends HTMLElement {
       }
     });
     this.content.innerHTML = `${lineString}\n`;
-    await sleep(300);
+    if (this.completedFirstWrite) {
+      await sleep(300);
+    }
     this.allowBolds = hitRandom(4);
     this.allowEmphasis = hitRandom(7);
     for (let lineIndex = 0; lineIndex < theLines.length; lineIndex += 1) {
       const words = theLines[lineIndex].split(' ');
       await this.outputWords(words);
       this.content.innerHTML += "\n";
-      await sleep(60);
+      if (this.completedFirstWrite) {
+        await sleep(60);
+      }
     }
-    await sleep(200);
+    if (this.completedFirstWrite) {
+      await sleep(200);
+    }
+    this.completedFirstWrite = true;
   }
 
   async outputWords(words) {
